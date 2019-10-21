751715f83317f52d3a8071b6330cb31788a2fdd2
Gatrrr <davidgatrmacaulay@gmail.com>
2019-10-21 14:10:39 -0700

    Create Event Form Built

--- src/components/CreateEventForm.js
+++ src/components/CreateEventForm.js
@@ -23,7 +23,55 @@ function CreateEventForm(props) {
   .get()
   .then(res =>
     console.log(res))
-    .catch(err => console.log("Error", err))
+  .catch(err => console.log("Error", err));
+
+  setInput({
+    guestNumber: "",
+    date: "",
+    budget: "",
+    entertainment: "",
+    shoppingList: ""
+  });
+
+  return (
+    <form onSubmit={submitForm}>
+      <Input 
+      type="text"
+      name="guestNumber"
+      onChange={handleChange}
+      placeholder="Enter Amount of Guests Attending"
+      value={input.guestNumber}
+      />
+      <Input 
+      type="text"
+      name="date"
+      onChange={handleChange}
+      placeholder="Enter The Date of the Party"
+      value={input.date}
+      />
+      <Input 
+      type="text"
+      name="budget"
+      onChange={handleChange}
+      placeholder="Enter the Budget of the Party"
+      value={input.budget}
+      />
+      <Input 
+      type="text"
+      name="entertainment"
+      onChange={handleChange}
+      placeholder="Enter what Entertainment will be at the Party"
+      value={input.entertainment}
+      />
+      <Input 
+      type="text"
+      name="shoppingList"
+      onChange={handleChange}
+      placeholder="Enter what is needed for the Party"
+      value={input.shoppingList}
+      />
+    </form>
+  )
   
 }
 
