0e63fef86a778034971b8b282dabf3d56b257979
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-10-22 16:56:34 -0500

    Forms Style with Style Component library

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -2,6 +2,30 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { registerUser } from "../actions";
 import { Link } from "react-router-dom";
+import styled from 'styled-components'
+
+
+//  Styled components
+export const FormWrapDiv = styled.div `
+    max-width: 600px;
+    margin: 50px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+  
+    `
+
+  export const RegForm = styled.form `
+  
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  `
+
+//  End Styled Component
+
 
 function RegisterForm(props) {
   const [input, setInput] = useState({});
@@ -19,11 +43,12 @@ function RegisterForm(props) {
     });
   };
   return (
-    <>
-      <h1>RegisterForm</h1>
+    <FormWrapDiv className = "FormWrap">
+      <h1>Register  </h1>
       {props.isLogging && <div>Loading</div>}
 
-      <form onSubmit={submitForm}>
+      <RegForm onSubmit={submitForm}>
+      <label>Username</label>
         <input
           type="text"
           name="username"
@@ -31,6 +56,8 @@ function RegisterForm(props) {
           placeholder="Enter Username"
           value={input.username}
         />
+
+        <label>Password</label>
         <input
           type="password"
           name="password"
@@ -39,14 +66,14 @@ function RegisterForm(props) {
           value={input.password}
         />
 
-        <button>Submit</button>
-      </form>
-      <Link to="/">
-        <button>Back</button>
+        <button className = "submitbtn">Register</button>
+      </RegForm>
+      <Link to="/" className = "backbtn" >
+        <button>return</button>
       </Link>
 
       {props.error && <div>{props.error.message}</div>}
-    </>
+    </FormWrapDiv>
   );
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 function Welcome(props) {
   return (
     <div className="welcome">
-      <h1>Welcome</h1>
-      <button onClick={() => props.history.push("/register")}>Register</button>
-      <button onClick={() => props.history.push("/signin")}>Sign in</button>
+      <h1 className = "wel-h1">Welcome</h1>
+      <div className = "welcom-links">
+        <Link to = "/register" onClick={() => props.history.push("/register")}>Register</Link> <p> or </p> 
+        <Link  to = "/signin" onClick={() => props.history.push("/signin")}>Log In</Link>
+      </div>
+      {/* <button onClick={() => props.history.push("/register")}>Register</button>
+      <button onClick={() => props.history.push("/signin")}>Sign in</button> */}
     </div>
   );
 }
