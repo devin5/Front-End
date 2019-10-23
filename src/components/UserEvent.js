fd18e51e7de0c6f3355fae404525036093d675a1
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-10-23 17:44:09 -0500

    style

--- src/components/UserEvent.js
+++ src/components/UserEvent.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import React from 'react';
 import Styled from 'styled-components'
 
@@ -26,33 +26,14 @@ const CardDiv = Styled.div `
 
 const UserPage = props => {
   return (
-    // <CardDiv className="userpage">
-    //   <h1>{title}</h1>
-    //   <p>Number of Guests: </p><h3>{props.guestNumber}</h3>
-    //   <p>Date of your Party:</p> <h3>{props.date}</h3>
-    //   <p>Your Party budget:</p> <h3>{props.budget}</h3>
-    //   <p>The entertainment for your Party: </p><h3>{props.entertainment}</h3>
-    //   <p>The items needed for your Party: </p><h3>props.{shoppingList}</h3>
-    //   <button className = "submitbtn btn">Edite</button>
-    //   <button className = "submitbtn btn">Delete</button>
-    //  </CardDiv>
-    <>
-    </>
-=======
-import React from "react";
-
-const UserPage = props => {
-  return (
-    // <></>
-    <div className="userpage">
+    <CardDiv className="userpage">
       <h1>{props.index.title}</h1>
       Number of Guests: <h3>{props.index.guestNumber}</h3>
       Date of your Party: <h3>{props.index.date}</h3>
       Your Party budget: <h3>{props.index.budget}</h3>
       The entertainment for your Party: <h3>{props.index.entertainment}</h3>
       The items needed for your Party: <h3>{props.index.shoppingList}</h3>
-    </div>
->>>>>>> 0fa17f1679839f25416ecd23330161a39a1e9566
+    </CardDiv>
   );
 };
 
